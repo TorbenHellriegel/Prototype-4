eb77acb255a880963357da8e6e68780480747f37
TorbenHellriegel <torbenhellriegel@gmail.com>
2022-05-03 00:57:28 +0200

    Added powerup
    Added a powerup which pushes away enemys for a short time when collected

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,10 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
     private GameObject focalPoint;
+    public GameObject powerupIndicator;
 
+    public bool hasPowerupMulti = false;
+    private float powerupMultiStrength = 15;
     public float speed = 1;
     public float pushForce = 1;
 
@@ -20,6 +23,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Moves the powerup along with the player
+        powerupIndicator.gameObject.transform.position = new Vector3(transform.position.x, -0.5f, transform.position.z);
+
         // Move the player 
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
@@ -28,6 +34,40 @@ public class PlayerController : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Space))
         {
             playerRb.AddForce(focalPoint.transform.forward * forwardInput * pushForce, ForceMode.Impulse);
+        }
+    }
+
+    // Checks for trigges
+    private void OnTriggerEnter(Collider other)
+    {
+        // If the player collides with multiply powerup activate it
+        if(other.CompareTag("PowerupMulti"))
+        {
+            hasPowerupMulti = true;
+            powerupIndicator.gameObject.SetActive(true);
+            Destroy(other.gameObject);
+            StartCoroutine("PowerupCountdownRoutine");
+        }
+    }
+
+    // Counts down until the powerup dissapears
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerupMulti = false;
+        powerupIndicator.gameObject.SetActive(false);
+    }
+
+    // Checks for collisions
+    private void OnCollisionEnter(Collision collision)
+    {
+        // If player collides with enemy while having powerup repell enemy
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerupMulti)
+        {
+            Rigidbody enemyRigidbode = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPLayer = collision.gameObject.transform.position - transform.position;
+            enemyRigidbode.AddForce(awayFromPLayer * powerupMultiStrength, ForceMode.Impulse);
+            //gg
         }
     }
 }
---
+++ Assets/Scripts/PowerupRotate.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerupRotate : MonoBehaviour
+{
+    private float rotationSpeed = 100;
+    private float levitate = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        levitate += Time.deltaTime;
+
+        // Rotates the powerup
+        transform.Rotate(Vector3.up, Time.deltaTime * rotationSpeed);
+
+        // Makes the powerup float up and down
+        transform.position = new Vector3(transform.position.x, Mathf.Sin(levitate)/2 + 0.5f, transform.position.z);
+    }
+}
